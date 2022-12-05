4da27b95af1678e050bc63d1844b39dfba350f0a
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2022-12-05 17:53:54 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
+
