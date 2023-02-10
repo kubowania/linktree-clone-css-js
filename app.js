119f2db37a0e0ea1b1fc2c2f4fe03898eae85aca
ImiZocktTv <76225977+ImiZocktTv@users.noreply.github.com>
2023-02-10 16:09:49 +0100

    Some things changed
    - Overview improved
    - Fontawesome icons implemented
    - Transition reworked
    - Ukraine support badge added
    - Removed unnecessary content
    
    Co-Authored-By: Ania Kubow <51362610+kubowania@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
-//prevent button going to the site
+    //prevent button going to the site
     e.preventDefault()
     const link = this.getAttribute('link')
     console.log(link)
     try {
         await navigator.clipboard.writeText(link)
-        alert("Copied the text: " + link)
+        alert("Copied the URL: " + link)
     } catch (err) {
         console.error(err)
     }
